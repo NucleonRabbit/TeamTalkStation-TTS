dddb8ecae4c723db8f6e889cd9585d71c5f1340b
Nucleon14 <nucleon14@nucleon14.fedoradesktop>
2021-10-05 21:58:16 +0800

    Project Created

---
+++ TeamTalkStation-TTS_Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamTalkStation_TTS_Client.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ TeamTalkStation-TTS_Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace TeamTalkStation_TTS_Client.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
